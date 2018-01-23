798280f455e5f6625170d2f0cd89ebb3d6026748
liurongting <1025472263@qq.com>
2018-01-23 15:20:01 +0800

    初始化

---
+++ src/javascripts/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        	撒的撒
+      </div>
+    );
+  }
+}
+
+export default App;
