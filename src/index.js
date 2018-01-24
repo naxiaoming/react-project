b37ed471ac145a6e5fb79701338abc4638342563
liurongting <1025472263@qq.com>
2018-01-24 09:09:33 +0800

    合并

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './javascripts/App';
 import Home from './javascripts/AppMain/home';
+import TuanGou from './javascripts/tuangou/tuangou';
 import './stylesheets/index.scss';
 import registerServiceWorker from './registerServiceWorker';
 import {Router,Route,hashHistory,IndexRedirect,Redirect} from 'react-router'
@@ -9,6 +10,7 @@ let route=<Router history={hashHistory}>
 			<Route path='/' component={App}>
 				<IndexRedirect to="home" />
 				<Route path='/home' component={Home}/>
+				<Route path='/tuangou' component={TuanGou}/>
 				<Redirect from="*" to="home" />
 			</Route>		
 		</Router>
